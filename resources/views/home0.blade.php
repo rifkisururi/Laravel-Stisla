12b9a249b08536d4bc813a2c9fc9e9387f04a119
rifki ahmad sururi <rifkisururi27@gmail.com>
2021-06-19 11:24:35 +0700

    laravel stisla with auth

---
+++ resources/views/home0.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.template')
+
+@section('content')
+
+
+
+<div class="main-content">
+  <section class="section">
+    <div class="section-header">
+      <h1>Blank Page</h1>
+    </div>
+
+    <div class="section-body">
+      ini isinya
+    </div>
+  </section>
+</div>
+
+@endsection
